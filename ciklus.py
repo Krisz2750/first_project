872e22d761d5b01758091f6cf245ff2a5fd451c9
Python_training <szepek@ahrt.hu>
2021-10-13 16:26:57 +0200

    Krisz

--- ciklus.py
+++ ciklus.py
@@ -1,6 +1,15 @@
-i = 1
+# i = 1
+#
+# while i <= 20: #
+#     if (i*7) % 3 == 0:
+#         print(i, i*7,"*")
+#     else:
+#         print(i, i*7)
+#     i = i+1
 
-while i <= 20:
-    print(i, i*7)
-    i = i+1
-
+k = 1
+jel = "*"
+while k <= 7:
+    print("*" * k)
+    k +=1
+print("END")
--- ciklus_3.py
+++ ciklus_3.py
@@ -1,11 +1,12 @@
-i = 1
-k = 2
-l = 1
-while k <= 12:
-    l = k % 2
-    if l == 0:
-        i = i * 3
-        print(str(k) + ".:",i)
-    else:
-        print(str(k) + ".:", i)
-    k=k+1
+# i = 1
+# k = 2
+# l = 1
+# while k <= 12:
+#     l = k % 2
+#     if l == 0:
+#         i = i * 3
+#         print(str(k) + ".:",i)
+#     else:
+#         print(str(k) + ".:", i)
+#     k=k+1
+
